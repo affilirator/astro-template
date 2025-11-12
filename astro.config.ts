2a0c948c42dcc0fe450b86706ef091fffb38bd8e
Patrick Mahinge <109906110+affilirator@users.noreply.github.com>
2025-11-12 13:36:24 +0300

    Update site URL in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-
+  site: 'https://affilirator.github.io/astro-template/',
   integrations: [
     tailwind({
       applyBaseStyles: false,
